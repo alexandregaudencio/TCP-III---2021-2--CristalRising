f23164e17b319f0e77abfc242e0d3abd272ebc9d
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-07-20 21:53:32 -0300

    States: Abstract, walk and Idle.

--- Assets/Users/Ale/PlayerStates/IdleState.cs
+++ Assets/Users/Ale/PlayerStates/IdleState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playerx : MonoBehaviour
+public class IdleState : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
