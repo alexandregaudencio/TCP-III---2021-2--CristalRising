7a08c817080e334fa57dde209e10d345d44221e0
bruno-reload <bruno-reload@live.com>
2021-07-23 19:05:08 -0300

    primeira modelagem para habilidades

---
+++ Assets/Users/Bruno/Player/Combat/Magics/Spells.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Spells :CombatControl, Neutral, Deffencive
+{
+    public List<Status> status;
+    public Attibute attibut;
+    public new AnimationClip animation;
+    public virtual void BuildElement()
+    {
+        throw new System.NotImplementedException();
+    }
+    public virtual void Use()
+    {
+        throw new System.NotImplementedException();
+    }
+    public virtual void CallAnimation() { 
+    }
+}
---
+++ Assets/Users/Bruno/Player/Combat/Magics/Status/AbstractionStatus/Cure.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Spell/Cure", order = 1)]
+public class Cure : Status
+{
+    public float value;
+}
