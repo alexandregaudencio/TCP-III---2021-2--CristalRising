198e33d13aa4ad24d1aebfda445e31b95a6fab4c
sarahmattos <sahmattos13@gmail.com>
2021-08-02 15:55:03 -0300

    Nome duplicado part.2

--- Assets/Users/Romualdo/Scripts/LobbyController.cs
+++ Assets/Users/Romualdo/Scripts/LobbyController.cs
@@ -140,7 +140,7 @@ public class LobbyController: MonoBehaviourPunCallbacks
 
         }
          Player[] players = PhotonNetwork.PlayerList;
-         for (int i = 0; i < players.Count(); i++)
+         for (int i = 0; i < PhotonNetwork.CurrentRoom.Players.Count(); i++)
          {
              Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
         }
@@ -153,8 +153,8 @@ public class LobbyController: MonoBehaviourPunCallbacks
     public void QuitRoom()
     {
         PhotonNetwork.LeaveRoom();
-        this.RoomCanvas.SetActive(false);
-        this.LobbyCanvas.SetActive(true);
+       // this.RoomCanvas.SetActive(false);
+       // this.LobbyCanvas.SetActive(true);
     }
     public override void OnLeftRoom()
     {
