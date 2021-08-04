97bdd09b61701ca7cbeecf7b19bef370b0d9e75c
bruno-reload <bruno-reload@live.com>
2021-08-03 21:33:10 -0300

    arquivos duplicados em pastas aleatórias removidos

--- Assets/TextMesh Pro/Sprites/Player/Combat/Magics/Status/AbstractionStatus/StatusCure.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "Spell/Cure", order = 1)]
-public class StatusCure : Status
-{
-    public float value;
-}
