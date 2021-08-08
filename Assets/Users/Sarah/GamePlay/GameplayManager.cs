c7d36edd90a312b4423db07a11453a43b8a68807
sarahmattos <sahmattos13@gmail.com>
2021-08-08 16:40:56 -0300

    Tempo de gamePlay e pontuação na colissão com a área

---
+++ Assets/Users/Sarah/GamePlay/GameplayManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using System.Linq;
+
+public class GameplayManager : MonoBehaviour
+{
+    public TMP_Text timeToDisplay;
+
+    private bool endingGame = false;
+    //private PhotonView PV;
+    private TimerCountdown gameplayRoomTimer;
+    private void Start()
+    {
+        //PV = GetComponent<PhotonView>();
+        gameplayRoomTimer = GetComponent<TimerCountdown>();
+        gameplayRoomTimer.CurrentTime = RoomConfigs.gameplayMaxTime;
+
+    }
+
+    private void Update()
+    {
+        UIUpdate();
+
+        if (gameplayRoomTimer.IsCountdownOver())
+        {
+            if (endingGame) return;
+            EndGame();
+        }
+    }
+
+    private void UIUpdate()
+    {
+        string tempTimer = string.Format("{0:00}", gameplayRoomTimer.CurrentTime);
+        timeToDisplay.text = tempTimer;
+    }
+
+    public void EndGame()
+    {
+        endingGame = true;
+        Debug.Log("Acabou o jogo");
+        //aqui
+        //if (!PhotonNetwork.IsMasterClient) return;
+        ///PhotonNetwork.CurrentRoom.IsOpen = false;
+
+    }
+
+    /* public override void OnPlayerEnteredRoom(Player newPlayer)
+     {
+
+         if (PhotonNetwork.IsMasterClient)
+         {
+             PV.RPC("SendTimer", RpcTarget.Others, gameplayRoomTimer.CurrentTime);
+         }
+         base.OnPlayerEnteredRoom(newPlayer);
+     }
+
+     [PunRPC]
+     private void SendTimer(float timeIn)
+     {
+         gameplayRoomTimer.CurrentTime = timeIn;
+     }
+     */
+}
---
+++ Assets/Users/Sarah/GamePlay/PlayerTesteGameplaySarah.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerTesteGameplaySarah : MonoBehaviour
+{
+    public static PlayerTesteGameplaySarah instance ;
+    public float Movespeed = 4f;
+    private float Turnspeed = 120f;
+
+    public GameObject _objeto1; //Objeto 1
+    public GameObject _objeto2; //Objeto 2
+    public float _distance; //Armazena a distância
+    public bool areaCheck;
+    public float points;
+
+    public TMP_Text timeToDisplay;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        instance = this;
+        _distance =0;
+        areaCheck = false;
+        points = 0;
+    }
+    void Update()
+    {
+        Controls();
+        Distance();
+    }
+    private void Controls()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
+        this.transform.Translate(Vector3.forward * vert * Movespeed * Time.deltaTime);
+        this.transform.localRotation *= Quaternion.AngleAxis(horz * Turnspeed * Time.deltaTime, Vector3.up);
+    }
+    private void Distance()
+    {
+        _distance = Vector3.Distance(_objeto1.transform.position, _objeto2.transform.position); //Calculamos a distância e atribuimos a variável
+        //Debug.Log(_distance);
+    }
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("area"))
+        {
+            areaCheck = true;
+        }
+
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("area"))
+        {
+            areaCheck = false;
+        }
+    }
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("area"))
+        {
+            points+=Time.deltaTime;
+            //Debug.Log(points);
+            string tempTimer = string.Format("{0:00}", points);
+            timeToDisplay.text = tempTimer;
+        }
+    }
+
+
+}
