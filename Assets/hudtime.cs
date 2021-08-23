b501c1f2e2a7c60b612b3ea079e9bf56ca4f2435
Romualdo Rasuk <78970473+Rasuk@users.noreply.github.com>
2021-08-23 13:53:23 -0300

    Sistema de Hud indicando a que time o jogador pertence

---
+++ Assets/hudtime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hudtime : MonoBehaviour
+{
+    public Text aaaaaa;
+    void Start()
+    {
+        aaaaaa.text = GameObject.Find("PlayerAssister").GetComponent<PlayerAssister>().MyTeam.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
