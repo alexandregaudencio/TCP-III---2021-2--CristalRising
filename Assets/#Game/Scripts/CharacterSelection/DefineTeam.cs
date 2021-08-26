80c05652a365db0dafa221ccaddb05202aa3ed79
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-08-26 12:04:51 -0300

    "A  Limpa" no Menu e na seleção de personagens.

---
+++ Assets/#Game/Scripts/CharacterSelection/DefineTeam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun.UtilityScripts;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class DefineTeam : PhotonTeamsManager
+{
+
+    public void TeamDefinition()
+    {
+        for (int i = 0; i < PhotonNetwork.CurrentRoom.PlayerCount; i++)
+        {
+            if(i%2 == 0) PhotonNetwork.PlayerList[i].JoinTeam("Blue");
+            else         PhotonNetwork.PlayerList[i].JoinTeam("Red");
+        }
+    }
+
+}
--- Assets/#Game/Scripts/RoomConfigs.cs
+++ Assets/#Game/Scripts/RoomConfigs.cs
@@ -1,17 +1,18 @@
 ﻿public static class RoomConfigs
 {
     //CreateRoomC
-    public const int maxRoomPlayers = 2;
+    public const int maxRoomPlayers = 6;
+    public const int maxPlayersTeam = maxRoomPlayers / 2;
 
     //characterSelection
-    public const int characterSelectionMaxTime = 5;
+    public const int characterSelectionMaxTime = 20;
 
     //Gameplay
     public const int gameplayMaxTime = 60;
 
     //SceneIndex
     public const int menuSceneIndex = 0;
-    public const int CharacterSelectionSceneIndex = 1;
-    public const int gameplaySceneIndex = 4;
+    public const int CharSelecSceneIndex = 1;
+    public const int gameplaySceneIndex = 3;
 
 }
