523ed5d5f2a10cb8db67ec1445de930dec480bfd
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-08-27 19:02:46 -0300

    no message

---
+++ Assets/SetUpGameplay.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.IO;
+using System.Linq;
+using Photon.Pun.UtilityScripts;
+
+
+public class SetUpGameplay : MonoBehaviour
+{
+    private PhotonView PV;
+    [SerializeField] private GameObject[] SpawnPointsBlue, SpawnPointsRed;
+    [SerializeField] public GameObject[] Characters;
+
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+
+    void Start()
+    {
+        InstantiatingPlayersCharacter();
+
+    }
+
+    private void InstantiatingPlayersCharacter()
+    {
+
+            string pTeam = PhotonNetwork.LocalPlayer.GetPhotonTeam().Name;
+            
+
+            if (pTeam == "Blue")
+            {
+                PV.RPC("RPCInstantiateCharacter", PhotonNetwork.LocalPlayer, SpawnPointsBlue[0].transform.position, SpawnPointsBlue[0].transform.rotation);
+                //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), 
+                //SpawnPointsBlue[0].transform.position, SpawnPointsBlue[0].transform.rotation, 0);
+            }
+            if (pTeam == "Red")
+            {
+                PV.RPC("RPCInstantiateCharacter", PhotonNetwork.LocalPlayer, SpawnPointsRed[0].transform.position, SpawnPointsRed[0].transform.rotation);
+            }
+
+     
+    }
+
+    [PunRPC]
+    void RPCInstantiateCharacter(Vector3 spawnPos, Quaternion spawnRot)
+    {
+        //myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), spawnPos, spawnRot, 0);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), spawnPos, spawnRot);
+    }
+}
