71be4e97b8df8cd129b2f597ca46ea34f1e0a1f5
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-08-29 16:45:18 -0300

    Concluído seleção de personagens na cena oficial.

--- Assets/#Game/Scripts/RoomConfigs.cs
+++ Assets/#Game/Scripts/RoomConfigs.cs
@@ -5,7 +5,7 @@
     public const int maxPlayersTeam = maxRoomPlayers / 2;
 
     //characterSelection
-    public const int characterSelectionMaxTime = 3;
+    public const int characterSelectionMaxTime = 10;
 
     //Gameplay
     public const int gameplayMaxTime = 60;
--- Assets/Users/Ale/PlayersCharactersVisualizing.cs
+++ Assets/Users/Ale/PlayersCharactersVisualizing.cs
@@ -13,35 +13,51 @@ public class PlayersCharactersVisualizing : MonoBehaviourPunCallbacks
     [SerializeField] private Image[] PlayersCharacterIcon;
     [SerializeField] private TMP_Text[] PlayersTextName;
 
+    [SerializeField] private Sprite[] charactersIconToSet;
+
     private Player[] myTeamMembers;
+    private PhotonTeam myTeam;
 
     // Start is called before the first frame update
     void Start()
     {
-        PhotonTeam myTeam = PhotonTeamExtensions.GetPhotonTeam(PhotonNetwork.LocalPlayer);
-
+        myTeam = PhotonTeamExtensions.GetPhotonTeam(PhotonNetwork.LocalPlayer);
         PhotonTeamsManager.Instance.TryGetTeamMembers(myTeam.Code, out myTeamMembers);
+
 
         for (int i = 0; i < myTeamMembers.Length; i++)
         {
             PlayersTextName[i].text =  myTeamMembers[i].NickName;
+            PlayersCharacterIcon[i].sprite =  charactersIconToSet[0];
         }
-  
 
     }
-    
-    // Update is called once per frame
-    void Update()
+
+    //void UpdatePlayersTeamVisualizing()
+    //{
+    //    for (int i = 0; i < myTeamMembers.Length; i++)
+    //    {
+    //        PlayersTextName[i].text = myTeamMembers[i].NickName;
+    //        PlayersCharacterIcon[i].sprite = (Sprite)myTeamMembers[i].CustomProperties["characterIcon"];
+    //    }
+
+    //}
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
     {
-        
+        for (int i = 0; i < myTeamMembers.Length; i++)
+        {
+            if(myTeamMembers[i] == targetPlayer)
+            {
+                int indexImg = (int)targetPlayer.CustomProperties["characterIcon"];
+                PlayersCharacterIcon[i].sprite = charactersIconToSet[indexImg];
+                //PlayersTextName[i].text = myTeamMembers[i].NickName;
+            }
+        }        
+       
+        //UpdatePlayersTeamVisualizing();
+
     }
-
-    public void PlayerChoosing(GameObject CharacterPrefab)
-    {
-
-    }
-
-
 
 }
 
