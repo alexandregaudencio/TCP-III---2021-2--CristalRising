300395d7109b04bf25af6a84ffbedbfd1e350ff9
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-08-30 07:55:33 -0300

    Importação de um pacote provisório e ajuste na cena de seleção.

--- Assets/#Game/Scripts/CharacterSelection/ButtonsCharactSelectManager.cs
+++ Assets/#Game/Scripts/CharacterSelection/ButtonsCharactSelectManager.cs
@@ -9,12 +9,9 @@ using ExitGames.Client.Photon;
 
 public class ButtonsCharactSelectManager : MonoBehaviour
 {
-    //Pra dar bom, os parâmetros abaixo precisam estar na mesma ordem.
-    
+    //Pra dar bom, a ordem de associação dos objetos via unity precisam estar na mesma ordem.
     [SerializeField] private Button[] characterSelectButtons;
-
     [SerializeField] private GameObject CharacterDefault;   
-
     [SerializeField] private GameObject cancelButton;
 
     private ExitGames.Client.Photon.Hashtable characterIcon = new ExitGames.Client.Photon.Hashtable();
@@ -22,50 +19,21 @@ public class ButtonsCharactSelectManager : MonoBehaviour
     private void Start()
     {
         PhotonNetwork.LocalPlayer.TagObject = CharacterDefault.name;
-
-        //for (int i = 0; i < charactersIcons.Length; i++)
-        //{
-        //    PrefabIconName.Add(, charactersIcons[i].name);
-        //}
     }
 
-    private void Update()
-    {
-        
-    }
-
-    //private void OnHeighLightButtonActive()
-    //{
-    //    foreach(Button buttons in characterSelectButtons)
-    //    {
-    //        //buttons.OnPointerEnter;
-            
-    //    }
-    //}
 
     public void ChooseCharacter(GameObject characterPrefab)
     {
         PhotonNetwork.LocalPlayer.TagObject = characterPrefab.name;
         SwitchButtonsInteractable(false);
-
     }
 
+    //AÇÃO DOS BOTÕES DE SEÇÃO DE PERSONAGENS
     public void SwitchingIcon(int indexImgIcon)
     {
         characterIcon["characterIcon"] = indexImgIcon;
         PhotonNetwork.LocalPlayer.SetCustomProperties(characterIcon) ;
-        //foreach (Button button in characterSelectButtons)
-        //{
-        //    //if (button.OnPointerEnter();
-        //}
     }
-
-
-    public void ResetChange()
-    {
-        SwitchButtonsInteractable(true);
-    }
-
 
     private void SwitchButtonsInteractable(bool boolean)
     {
@@ -76,15 +44,6 @@ public class ButtonsCharactSelectManager : MonoBehaviour
 
         cancelButton.SetActive(!boolean);
     }
-
-    //public Image chos(Image imageIcon)
-    //{
-    //    return (imageIcon);
-        
-    //}
-
-
-
 
 }
 
--- Assets/#Game/Scripts/CharacterSelection/PlayersCharactersVisualizing.cs
+++ Assets/#Game/Scripts/CharacterSelection/PlayersCharactersVisualizing.cs
@@ -18,7 +18,6 @@ public class PlayersCharactersVisualizing : MonoBehaviourPunCallbacks
     private Player[] myTeamMembers;
     private PhotonTeam myTeam;
 
-    // Start is called before the first frame update
     void Start()
     {
         myTeam = PhotonTeamExtensions.GetPhotonTeam(PhotonNetwork.LocalPlayer);
@@ -30,18 +29,7 @@ public class PlayersCharactersVisualizing : MonoBehaviourPunCallbacks
             PlayersTextName[i].text =  myTeamMembers[i].NickName;
             PlayersCharacterIcon[i].sprite =  charactersIconToSet[0];
         }
-
     }
-
-    //void UpdatePlayersTeamVisualizing()
-    //{
-    //    for (int i = 0; i < myTeamMembers.Length; i++)
-    //    {
-    //        PlayersTextName[i].text = myTeamMembers[i].NickName;
-    //        PlayersCharacterIcon[i].sprite = (Sprite)myTeamMembers[i].CustomProperties["characterIcon"];
-    //    }
-
-    //}
 
     public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
     {
@@ -51,12 +39,9 @@ public class PlayersCharactersVisualizing : MonoBehaviourPunCallbacks
             {
                 int indexImg = (int)targetPlayer.CustomProperties["characterIcon"];
                 PlayersCharacterIcon[i].sprite = charactersIconToSet[indexImg];
-                //PlayersTextName[i].text = myTeamMembers[i].NickName;
             }
         }        
        
-        //UpdatePlayersTeamVisualizing();
-
     }
 
 }
