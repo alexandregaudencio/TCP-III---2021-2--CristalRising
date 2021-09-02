1fdd04e0d3530e1167e3afffc897936f39915914
sarahmattos <sahmattos13@gmail.com>
2021-09-02 20:15:36 -0300

    Base que abre após algum tempo

--- Assets/#Game/Scripts/TimerCountdown.cs
+++ Assets/#Game/Scripts/TimerCountdown.cs
@@ -6,12 +6,18 @@ public class TimerCountdown : MonoBehaviour
 {
     float maxTime;
     float currentTime;
+    float timePassed;
 
     public float CurrentTime { get => currentTime; set => currentTime = value; }
-
+   
     public bool IsCountdownOver()
     {
         return (CurrentTime > 0.0f) ? false : true;
+    }
+    public bool IsCountdownStart()
+    {
+       timePassed = RoomConfigs.gameplayMaxTime - CurrentTime ;
+        return (timePassed < 5) ? false : true;
     }
 
     private void FixedUpdate()
