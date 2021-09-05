fd26dbdc82d776741159da6a106b9705932cb0d7
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-09-05 16:11:15 -0300

    Outline com cor do time e ajustes na posição da arma do personagem

---
+++ Assets/Users/Ale/SetOutlineColor.cs
@@ -0,0 +1,47 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+
+public class SetOutlineColor : MonoBehaviour
+{
+    byte teamCode;
+    Outline outline;
+    private Color TeamColor => (teamCode == 1) ? RoomConfigs.instance.blueTeamColor : RoomConfigs.instance.redTeamColor;
+    PhotonView PV;
+
+    Player[] playersTeamBlue;
+    Player[] playersTeamRed;
+
+
+    void Start()
+    {
+        outline = GetComponent<Outline>();
+        //PV = GetComponent<PhotonView>();
+        teamCode = PhotonNetwork.LocalPlayer.GetPhotonTeam().Code;
+
+
+        PhotonTeamsManager.Instance.TryGetTeamMembers("Blue", out playersTeamBlue);
+        PhotonTeamsManager.Instance.TryGetTeamMembers("Red", out playersTeamRed);
+
+        foreach (Player p in playersTeamBlue)
+            if (GetComponent<PhotonView>().Controller.Equals(p))
+            {
+                GetComponent<Outline>().OutlineColor = RoomConfigs.instance.blueTeamColor;
+                //teamIdentify.GetComponent<Renderer>().material.SetColor(RoomConfigs.instance.blueTeamColor);
+                gameObject.layer = LayerMask.NameToLayer("Team1");
+            }
+        foreach (Player p in playersTeamRed)
+            if (GetComponent<PhotonView>().Controller.Equals(p))
+            {
+                GetComponent<Outline>().OutlineColor = RoomConfigs.instance.redTeamColor;
+                //teamIdentify.GetComponent<Renderer>().material.SetColor(RoomConfigs.instance.redTeamColor);
+                gameObject.layer = LayerMask.NameToLayer("Team2");
+            }
+    }
+
+
+
+}
