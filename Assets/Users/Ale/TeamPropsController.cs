5de70ea7e4c7fcfa8882633d4adb4389dd79bbaf
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-09-06 18:00:25 -0300

    no message

---
+++ Assets/Users/Ale/TeamPropsController.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamPropsController : MonoBehaviour
+{
+    //byte localTeam;
+    //Player[] playersTeamBlue;
+    //Player[] playersTeamRed;
+
+    //public Player[] PlayersTeamBlue { get => playersTeamBlue; set => playersTeamBlue = value; }
+    //public Player[] PlayersTeamRed { get => playersTeamRed; set => playersTeamRed = value; }
+
+    //private Color TeamColor => (localTeam == 1) ? RoomConfigs.instance.blueTeamColor : RoomConfigs.instance.redTeamColor;
+
+    //public static TeamPropsController instance;
+
+    //private void Start()
+    //{
+    //    instance = this;
+
+    //    localTeam = PhotonNetwork.LocalPlayer.GetPhotonTeam().Code;
+
+    //    PhotonTeamsManager.Instance.TryGetTeamMembers("Blue", out playersTeamBlue);
+    //    PhotonTeamsManager.Instance.TryGetTeamMembers("Red", out playersTeamRed);
+
+
+    //}
+
+
+
+
+}
