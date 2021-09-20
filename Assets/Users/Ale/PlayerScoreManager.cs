69711da6a823c3fcbaf98ae5241020a1c38dd306
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-09-20 17:55:34 -0300

    script para a pontuação dos players

---
+++ Assets/Users/Ale/PlayerScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoreManager : MonoBehaviourPunCallbacks
+{
+    private ExitGames.Client.Photon.Hashtable HashProps = new ExitGames.Client.Photon.Hashtable();
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (PhotonNetwork.LocalPlayer == targetPlayer)
+        {
+            if(changedProps.ContainsKey("HP") && (int)targetPlayer.CustomProperties["HP"] <= 0)
+            {
+
+                HashProps["isDead"] = true;
+                HashProps["deathCount"] = (string)targetPlayer.CustomProperties["deathCount"] + 1;
+                PhotonNetwork.LocalPlayer.SetCustomProperties(HashProps);
+            }
+
+        }
+
+    }
+
+
+}
