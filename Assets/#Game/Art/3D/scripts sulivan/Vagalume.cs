a9ff62052951f93d539479bb61bc0f39c44b749d
Sulivan Sergio <76742666+sulivansergio@users.noreply.github.com>
2021-09-29 18:02:08 -0300

    Susu fez algo :) (cenario, vagalumes e colisores)

---
+++ Assets/#Game/Art/3D/scripts sulivan/Vagalume.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vagalume : MonoBehaviour
+{
+
+    public GameObject objeto;
+   
+    Movimento movimento;
+
+    public float speed;
+    public float tempo;
+    public float tempoFinal;
+
+
+    void Start()
+    {
+
+        movimento = new Movimento(objeto, objeto.transform.position, speed, tempo, tempoFinal);
+
+    }
+
+    
+    void Update()
+    {
+
+        movimento.Update(Time.deltaTime);
+
+
+    }
+
+
+
+
+}
